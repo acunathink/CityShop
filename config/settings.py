a42fb7c6c3a8b4cedd03aca4b4df3506afc37eb6
Тимофей Карпов <acuna-think@yandex.ru>
2024-07-21 07:42:23 +0300

    add Dockerfile and docker-compose.yml

--- config/settings.py
+++ config/settings.py
@@ -66,9 +66,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
@@ -84,10 +81,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
@@ -96,10 +89,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "collected_static"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATIC_URL = '/backend/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
