bae8b983dca3e19062c223b2098a3518442dc455
Тимофей Карпов <acuna-think@yandex.ru>
2024-07-21 03:44:16 +0300

    show related names at ShopSerializer responce

--- api/serializers.py
+++ api/serializers.py
@@ -11,13 +11,23 @@ class CitySerializer(serializers.ModelSerializer):
 
 
 class ShopSerializer(serializers.ModelSerializer):
-
+    city = serializers.PrimaryKeyRelatedField(source='city.name',
+                                              queryset=City.objects)
+    street = serializers.PrimaryKeyRelatedField(source='street.name',
+                                                queryset=Street.objects)
     class Meta:
+
         model = Shop
         fields = '__all__'
 
     def get_is_open(self, obj: Shop):
         return obj.is_open
+
+    def create(self, validated_data):
+        print(validated_data)
+        validated_data['city'] = validated_data.pop('city')['name']
+        validated_data['street'] = validated_data.pop('street')['name']
+        return Shop.objects.create(**validated_data)
 
 
 class StreetSerializer(serializers.ModelSerializer):
